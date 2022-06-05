3c1a35654ac03727227465d08a1e73609444771f
Toby Smith <tobysmith568@hotmail.co.uk>
2022-06-05 19:44:06 +0100

    Adjust the GitHub getIdForUserOrOrg GQL query

--- src/github/gql/get-id-for-user-or-org.ts
+++ src/github/gql/get-id-for-user-or-org.ts
@@ -3,10 +3,7 @@ import { getClient } from "./gql-client";
 
 const getIdForUserOrOrgQuery = gql`
   query GetIdForUserOrOrg($login: String!) {
-    organization(login: $login) {
-      id
-    }
-    user(login: $login) {
+    repositoryOwner(login: $login) {
       id
     }
   }
@@ -17,23 +14,18 @@ interface GetIdForUserOrOrgVariables {
 }
 
 interface GetIdForUserOrOrgResponse {
-  organization: { id: string } | null;
-  user: { id: string } | null;
+  repositoryOwner: { id: string } | null;
 }
 
 export const getIdForUserOrOrg = async (accessToken: string, login: string) => {
   const variables: GetIdForUserOrOrgVariables = { login };
-  const result = await getClient(accessToken, true).request<
+  const result = await getClient(accessToken).request<
     GetIdForUserOrOrgResponse,
     GetIdForUserOrOrgVariables
   >(getIdForUserOrOrgQuery, variables);
 
-  if (result.organization) {
-    return result.organization.id;
-  }
-
-  if (result.user) {
-    return result.user.id;
+  if (result.repositoryOwner?.id) {
+    return result.repositoryOwner?.id;
   }
 
   throw new Error(`Could not find user or organization with login ${login}`);
